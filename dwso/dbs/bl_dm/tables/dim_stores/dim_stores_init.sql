75d855e6f4fc5a54367d6b55f0abfec7e41cbeaf
ELENA-VM <Elena.Mozhenkova@gmail.com>
2022-02-17 16:15:23 +0300

    fix bugs

--- dwso/dbs/bl_dm/tables/dim_stores/dim_stores_init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO  dim_stores(store_surr_id, store_id, source_system, source_entity, store_name, store_type_id, store_type, update_dt, insert_dt) 
-SELECT -1, -1, 'NA', 'NA', 'NA', -1, 'NA', GETDATE(), GETDATE()
-WHERE NOT EXISTS ( SELECT store_surr_id
-                   FROM dim_stores
-                   WHERE store_surr_id = -1 );
--- dwso/dbs/bl_dm/tables/dim_type_payments/dim_type_payments_init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO dim_type_payments(type_payment_surr_id, type_payment_id, source_system, source_entity, type_payment, update_dt, insert_dt) 
-SELECT -1, -1, 'NA', 'NA', 'NA', GETDATE(), GETDATE()
-WHERE NOT EXISTS ( SELECT type_payment_surr_id
-                   FROM dim_type_payments
-                   WHERE type_payment_surr_id = -1 );
